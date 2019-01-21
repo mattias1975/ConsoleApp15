ad3c5e6f46d07ce98be70be32138fb3201f94cd7
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-01-21 09:01:00 +0100

    Method

--- ConsoleApp15/Program.cs
+++ ConsoleApp15/Program.cs
@@ -8,13 +8,7 @@ namespace ConsoleApp15
         static void Main(string[] args)
         {
             //här nere deklarerar jag varibler som man använder
-            string tal1;
-            string tal2;
-            string summa;
-            int tal12;
-
-            int tal22;
-            int summan;
+            
             var keepAlive = true;// sätter keepalive till true
             while (keepAlive)//startar en loop till keepAlive är false 
             {
@@ -22,74 +16,36 @@ namespace ConsoleApp15
                 {
                     Console.WriteLine("Vilket räknesätt vill du räkna? + - * /"); //fråga skrivs på skärmen vilket räknestäss som du vill räkna
                     string Choice = (Console.ReadLine() ?? "");//läser in ditt val till varibeln Choise
-            
+
                     {
 
-                    }
-                
 
+
+                    }
+                                                         
                     switch (Choice) //startar Switchen
                     {
                         case "+"://tar in om man valt +
-                           
 
 
-                            Console.WriteLine("Skriv tal1");//frågar om första talet du vill räkna
-                            tal1 = Console.ReadLine();//hämtar in talet man matat in o läger i varibel tal1
-                            tal12 = Convert.ToInt32(tal1);// conventerar tal1 till interger så man kan räkna o läger tal12
+                          plus();//skickar ner en till metoden plus
+                            break;
 
-                            Console.WriteLine("skriv tal 2");//frågar efter tal 2
-                            tal2 = Console.ReadLine();//läser in tal2
-                            tal22 = Convert.ToInt32(tal2); //conventerar tal2 till interget och lägger i tal22
-                            summan = tal12 + tal22; //räknar ut summan av tal1 och tal 2
-                            summa = Convert.ToString(summan); //conventerar summan till en string 
-                            Console.WriteLine("Summan är" + " " + summa);//skriver ut summan är och sen summan
-                            Console.ReadKey();//gör så konsolen blir öppen till man tryckt på en knapp
-                            break;//avslutar detta case
 
-                             case "-"://case - startar om man väljer -
-                           
-                            Console.WriteLine("Skriv tal1");//ber dig skriva tal1
-                            tal1 = Console.ReadLine();//läser in tal1
-                            tal12 = Convert.ToInt32(tal1);//conventerar tal1 till interger och lägger i tal12
 
-                            Console.WriteLine("skriv tal 2");
-                            tal2 = Console.ReadLine();
-                            tal22 = Convert.ToInt32(tal2);
-                            summan = tal12 - tal22;
-                            summa = Convert.ToString(summan);
-                            Console.WriteLine("Differensen är" + " " + summa);
-                            Console.ReadKey();
+                        case "-"://case - startar om man väljer -
+
+                            minus();//skickar ner till metoden minus
                             break;
 
                         case "*":
 
-                            Console.WriteLine("Skriv tal1");
-                            tal1 = Console.ReadLine();
-                            tal12 = Convert.ToInt32(tal1);
-
-                            Console.WriteLine("skriv tal 2");
-                            tal2 = Console.ReadLine();
-                            tal22 = Convert.ToInt32(tal2);
-                            summan = tal12 * tal22;
-                            summa = Convert.ToString(summan);
-                            Console.WriteLine("Produkten är" + " " + summa);
-                            Console.ReadKey();
+                            ganger();
                             break;
 
                         case "/":
 
-                            Console.WriteLine("Skriv tal1");
-                            tal1 = Console.ReadLine();
-                            tal12 = Convert.ToInt32(tal1);
-
-                            Console.WriteLine("skriv tal 2");
-                            tal2 = Console.ReadLine();
-                            tal22 = Convert.ToInt32(tal2);
-                            summan = tal12 / tal22;
-                            summa = Convert.ToString(summan);
-                            Console.WriteLine("Kvoten är" + " " + summa);
-                            Console.ReadKey();
+                            delat();
                             break;
 
                         case "Q":
@@ -116,14 +72,110 @@ namespace ConsoleApp15
 
                     Console.ResetColor();
                 }
-               
-                
-          
+
+
+
+
+
+
 
             }
+            plus();
+        }
 
+        private static void plus()
+       
+         
+            {
+            string tal1;
+            string tal2;
+            string summa;
+            double tal12;
+
+            double tal22;
+            double summan;
+            Console.WriteLine("Skriv tal1");//ber dig skriva tal1
+            tal1 = Console.ReadLine();//läser in tal1
+            tal12 = Convert.ToInt32(tal1);//conventerar tal1 till interger och lägger i tal12
+
+            Console.WriteLine("skriv tal 2");
+            tal2 = Console.ReadLine();
+            tal22 = Convert.ToInt32(tal2);
+            summan = tal12 + tal22;
+            summa = Convert.ToString(summan);
+            Console.WriteLine("Differensen är" + " " + summa);
+            Console.ReadKey();
         }
+        private static void minus()
+
+
+        {
+            string tal1;
+            string tal2;
+            string summa;
+            double tal12;
+
+            double tal22;
+            double summan;
+            Console.WriteLine("Skriv tal1");//ber dig skriva tal1
+            tal1 = Console.ReadLine();//läser in tal1
+            tal12 = Convert.ToInt32(tal1);//conventerar tal1 till interger och lägger i tal12
+
+            Console.WriteLine("skriv tal 2");
+            tal2 = Console.ReadLine();
+            tal22 = Convert.ToInt32(tal2);
+            summan = tal12 - tal22;
+            summa = Convert.ToString(summan);
+            Console.WriteLine("Differensen är" + " " + summa);
+            Console.ReadKey();
         }
+        private static void delat()
+
+
+        {
+            string tal1;
+            string tal2;
+            string summa;
+            double tal12;
+
+            double tal22;
+            double summan;
+            Console.WriteLine("Skriv tal1");//ber dig skriva tal1
+            tal1 = Console.ReadLine();//läser in tal1
+            tal12 = Convert.ToInt32(tal1);//conventerar tal1 till interger och lägger i tal12
+
+            Console.WriteLine("skriv tal 2");
+            tal2 = Console.ReadLine();
+            tal22 = Convert.ToInt32(tal2);
+            summan = tal12/tal22;
+            summa = Convert.ToString(summan);
+            Console.WriteLine("Differensen är" + " " + summa);
+            Console.ReadKey();
+        }
+        private static void ganger()
+
+
+        {
+            string tal1;
+            string tal2;
+            string summa;
+            double tal12;
+
+            double tal22;
+            double summan;
+            Console.WriteLine("Skriv tal1");//ber dig skriva tal1
+            tal1 = Console.ReadLine();//läser in tal1
+            tal12 = Convert.ToInt32(tal1);//conventerar tal1 till interger och lägger i tal12
+
+            Console.WriteLine("skriv tal 2");
+            tal2 = Console.ReadLine();
+            tal22 = Convert.ToInt32(tal2);
+            summan = tal12 * tal22;
+            summa = Convert.ToString(summan);
+            Console.WriteLine("Differensen är" + " " + summa);
+            Console.ReadKey();
+        }
+    }
     }
 
 
